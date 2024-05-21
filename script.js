dbbcf940bfb03b06f261d8b9829ea97f640a226b
EmelKrist <kristina_1516@mail.ru>
2024-05-21 23:56:59 +0800

    Add a drawing of the first tetromino

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 import { Tetris } from "./tetris.js";
+import { convertPositionToIndex } from "./utilities.js";
 
 const tetris = new Tetris();
+const cells = document.querySelectorAll(".grid>div");
+
+draw();
+
+/**
+ * Method of drawing.
+ */
+function draw() {
+  // clear class attribute of div's
+  cells.forEach((cell) => cell.removeAttribute("class"));
+  drawTetromino();
+}
+
+/**
+ * Method to draw a tetromino.
+ */
+function drawTetromino() {
+  // get name and size of tetromino
+  const name = tetris.tetromino.name;
+  const tetrominoMatrixSize = tetris.tetromino.matrix.length;
+  for (let row = 0; row < tetrominoMatrixSize; row++) {
+    for (let column = 0; column < tetrominoMatrixSize; column++) {
+      // if matrix point is 0 then skip
+      if (!tetris.tetromino.matrix[row][column]) continue;
+      // if it isn't visible then skip
+      if (tetris.tetromino.row + row < 0) continue;
+      // convert the position to cell index
+      const cellIndex = convertPositionToIndex(
+        tetris.tetromino.row + row,
+        tetris.tetromino.column + column
+      );
+      /* add tetromino name to classlist of cell 
+      with the converted index */
+      cells[cellIndex].classList.add(name);
+    }
+  }
+}
--- tetris.js
+++ tetris.js
@@ -1,18 +1,50 @@
-import { PLAYFIELD_COLUMS, PLAYFIELD_ROWS } from "./utilities.js";
+import {
+  PLAYFIELD_COLUMS,
+  PLAYFIELD_ROWS,
+  TETROMINO_NAMES,
+  TETROMINOS,
+  getRandomElement,
+} from "./utilities.js";
 
 export class Tetris {
   constructor() {
     this.playfield;
+    this.tetromino;
     this.init();
   }
 
+  /**
+   * Init method.
+   */
   init() {
     this.generatePlayfield();
+    this.generateTetromino();
   }
 
+  /**
+   * Method to generate the play field.
+   */
   generatePlayfield() {
     this.playfield = new Array(PLAYFIELD_ROWS)
       .fill()
       .map(() => new Array(PLAYFIELD_COLUMS).fill(0));
+  }
+
+  /**
+   * Method to generate a tetromino.
+   */
+  generateTetromino() {
+    const name = getRandomElement(TETROMINO_NAMES);
+    const matrix = TETROMINOS[name];
+
+    const column = PLAYFIELD_COLUMS / 2 - Math.floor(matrix.length / 2);
+    // const row = -2;
+    const row = 3;
+    this.tetromino = {
+      name,
+      matrix,
+      row,
+      column,
+    };
   }
 }
